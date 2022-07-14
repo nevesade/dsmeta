28439429eaa647685dd8d3a0f4d55d5349cd3c0e
nevesade <adenasiocarvalho19@gmail.com>
2022-07-14 22:09:56 +0100

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,21 @@
+import NotificationButon from "./components/NotificationButton"
 
 
 function App() {
-  
+
 
   return (
-    <h1>Olá dev!</h1>
+
+    <>
+      <h1>Olá dev!</h1>
+      <NotificationButon />
+      <NotificationButon />
+      <NotificationButon />
+      <NotificationButon />
+      <NotificationButon />
+      <NotificationButon />
+    </>
+
   )
 }
 
