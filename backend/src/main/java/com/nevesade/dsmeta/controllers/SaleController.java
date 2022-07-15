9ba82cda183b95eb414dcf09cc88452966a805ca
nevesade <adenasiocarvalho19@gmail.com>
2022-07-15 18:29:20 +0100

    Date select

--- backend/src/main/java/com/nevesade/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/nevesade/dsmeta/controllers/SaleController.java
@@ -1,10 +1,13 @@
 package com.nevesade.dsmeta.controllers;
 
-import java.util.List;
+import java.time.LocalDate;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nevesade.dsmeta.entities.Sale;
@@ -20,8 +23,11 @@ public class SaleController {
 	
 	
 	@GetMapping
-	public List<Sale> findSales() {
-		return service.findSales();
+	public Page<Sale> findSales( 
+		@RequestParam(value = "minDate", defaultValue = "") String minDate,  
+		@RequestParam(value = "maxDate", defaultValue = "") String maxDate, 
+		Pageable pageable) {
+	return service.findSales(minDate, maxDate, pageable);
 	}
 
 }
